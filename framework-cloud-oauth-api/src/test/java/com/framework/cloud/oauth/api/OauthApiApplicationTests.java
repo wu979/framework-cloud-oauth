6dcd193f0557d9d04dd0f101cc13282ba0d03ba7
吴思伟 <18683789594@163.com>
2022-06-16 19:13:22 +0800

    commit

---
+++ framework-cloud-oauth-api/src/test/java/com/framework/cloud/oauth/api/OauthApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.framework.cloud.oauth.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OauthApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
