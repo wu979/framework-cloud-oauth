6cdc8b88459fe65974a843ad272eefb643d3b55e
吴思伟 <18683789594@163.com>
2022-07-26 15:25:15 +0800

    commit

--- framework-cloud-oauth-api/src/main/java/com/framework/cloud/oauth/api/OauthApiApplication.java
+++ framework-cloud-oauth-api/src/main/java/com/framework/cloud/oauth/api/OauthApiApplication.java
@@ -2,9 +2,11 @@ package com.framework.cloud.oauth.api;
 
 import com.framework.cloud.core.annotation.FrameworkApplication;
 import com.framework.cloud.feign.annotation.EnableFeignInterceptor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@Slf4j
 @EnableFeignInterceptor
 @EnableDiscoveryClient
 @FrameworkApplication
@@ -12,6 +14,7 @@ public class OauthApiApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(OauthApiApplication.class, args);
+        log.info("Oauth service started successfully");
     }
 
 }
