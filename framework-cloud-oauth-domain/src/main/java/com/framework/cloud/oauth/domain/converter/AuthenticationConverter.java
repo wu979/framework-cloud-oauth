1eb7454f572689b8120543d197e6622a689a3824
吴思伟 <18683789594@163.com>
2022-08-05 09:11:53 +0800

    commit

--- framework-cloud-oauth-domain/src/main/java/com/framework/cloud/oauth/domain/converter/AuthenticationConverter.java
+++ framework-cloud-oauth-domain/src/main/java/com/framework/cloud/oauth/domain/converter/AuthenticationConverter.java
@@ -1,14 +1,18 @@
 package com.framework.cloud.oauth.domain.converter;
 
 import cn.hutool.core.collection.CollectionUtil;
+import com.framework.cloud.common.utils.FastJsonUtil;
+import com.framework.cloud.holder.constant.HeaderConstant;
 import com.framework.cloud.holder.constant.OauthConstant;
 import com.framework.cloud.holder.model.LoginUser;
 import com.framework.cloud.oauth.common.base.BaseUserDetail;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.security.oauth2.provider.token.DefaultUserAuthenticationConverter;
+import org.springframework.util.StringUtils;
 
 import java.util.Collection;
 import java.util.LinkedHashMap;
@@ -43,4 +47,35 @@ public class AuthenticationConverter extends DefaultUserAuthenticationConverter 
         return response;
     }
 
+    @Override
+    public Authentication extractAuthentication(Map<String, ?> map) {
+        Collection<? extends GrantedAuthority> authorities = this.getAuthorities(map);
+        if (map.containsKey(OauthConstant.USER_DETAIL)) {
+            Object principal = map.get(OauthConstant.USER_DETAIL);
+            LoginUser detail = FastJsonUtil.toJavaObject(principal, LoginUser.class);
+            return new UsernamePasswordAuthenticationToken(detail, OauthConstant.CREDENTIALS, authorities);
+        } else {
+            if (map.containsKey(HeaderConstant.X_USER_HEADER)) {
+                Object principal = map.get(HeaderConstant.X_USER_HEADER);
+                LoginUser loginUser = FastJsonUtil.toJavaObject(principal, LoginUser.class);
+                return new UsernamePasswordAuthenticationToken(loginUser, OauthConstant.CREDENTIALS, authorities);
+            }
+        }
+        return null;
+    }
+
+    private Collection<? extends GrantedAuthority> getAuthorities(Map<String, ?> map) {
+        if (!map.containsKey(OauthConstant.AUTHORITIES)) {
+            return null;
+        } else {
+            Object authorities = map.get(OauthConstant.AUTHORITIES);
+            if (authorities instanceof String) {
+                return AuthorityUtils.commaSeparatedStringToAuthorityList((String) authorities);
+            } else if (authorities instanceof Collection) {
+                return AuthorityUtils.commaSeparatedStringToAuthorityList(StringUtils.collectionToCommaDelimitedString((Collection) authorities));
+            } else {
+                throw new IllegalArgumentException("Authorities must be either a String or a Collection");
+            }
+        }
+    }
 }
